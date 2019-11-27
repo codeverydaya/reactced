af4d87eed87deb9f1f42293558c655de0bfce1df
lmy-hub <1013127768@qq.com>
2019-11-27 12:29:44 +0800

    change title

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import  {Button} from 'antd-mobile';
+import 'antd-mobile/dist/antd-mobile.css'
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button >天天敲代码</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
